58c830eb0683e560271da4e9a35bdc5c454c1f1e
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2013-09-18 13:52:52 +0300

    Add registration controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SimpleSupport::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-  devise_for :users, :controllers => { :sessions => 'users/sessions'}
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  }
+
   root 'dashboard#index'
 end
