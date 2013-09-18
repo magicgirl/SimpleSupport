e5b3dad19f53a89ea95bb0689add6dcaf6b7bb17
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2013-09-18 13:50:32 +0300

    Add bootstrap. Make login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SimpleSupport::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   devise_for :users, :controllers => { :sessions => 'users/sessions'}
   root 'dashboard#index'
 end
