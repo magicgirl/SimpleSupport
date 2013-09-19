47d67f4efd02ce91237dc3a4e9ce8d11d2f8f332
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2013-09-19 10:29:22 +0300

    Fix views. Add except_actions logic for active helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 module ApplicationHelper
 
-  def active(controller)
-    controller == params['controller'] ? 'active' : ''
+  def active(controller, args)
+    except_actions = args[:except_actions].present? ? args[:except_actions] : []
+    controller == params['controller'] and (not except_actions.include?(params['action'])) ? 'active' : ''
   end
 
   def sub_active(slug)
     "#{params['controller']}##{params['action']}" == slug ? 'active' : ''
   end
+
 end
